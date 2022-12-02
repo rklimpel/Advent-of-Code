b6584d4174202212a26f701179240793dca3f3fa
Rico Klimpel <rico@tklimpel.de>
2022-12-02 08:46:44 +0100

    clean fancy 1

--- 01/fancy.py
+++ 01/fancy.py
@@ -1,4 +1,2 @@
-import sys
-
 print('Task 1: ' + str(max(sum([int(c) for c in l.split("\n")]) for l in open("input.txt", "r").read().split("\n\n"))))
 print('Task 2: ' + str(sum(sorted(list([sum([int(c) for c in l.split("\n")]) for l in open("input.txt", "r").read().split("\n\n")]))[-3:])))
