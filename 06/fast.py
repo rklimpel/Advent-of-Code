3d871b37cfa907292ab7605cedc87862ffd63568
rklimpel <rico@tklimpel.de>
2022-12-06 07:54:52 +0100

    add day 6

---
+++ 06/fast.py
@@ -0,0 +1,12 @@
+chars = open("input.txt", "r").read()
+for i in range(1,(len(chars)-3)):
+    if len(set(chars[i:i+4])) == 4:
+        print(i+3+1)
+        print(chars[i:i+4])
+        break
+
+for i in range(1,(len(chars)-13)):
+    if len(set(chars[i:i+14])) == 14:
+        print(i+13+1)
+        print(chars[i:i+14])
+        break
