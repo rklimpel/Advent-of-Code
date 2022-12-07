106ef3b83ac6eb526e721f2e79365d818ae50b5d
Rico Klimpel <rico@tklimpel.de>
2022-12-07 13:44:26 +0100

    add 07 solution

--- 07/fast.py
+++ 07/fast.py
@@ -1,57 +1,68 @@
 import json
 
+TOTAL_FILESYSTEM = 70000000
+UPDATE_SIZE = 30000000
+
+def nested_set(dic, keys, value):
+    for key in keys[:-1]: dic = dic.setdefault(key, {})
+    dic[keys[-1]] = value
+
+def getFromDict(dataDict, mapList):    
+    for k in mapList: dataDict = dataDict[k]
+    return dataDict
+
 lines = open("input.txt", "r").read().split('\n')
 
 allFileSizes = 0
 filesystem = {}
-currentPath = ""
+allPaths = []
+currentPath = []
 for line in lines:
     if "$" in line:
         if "cd" in line:
             path = line[line.index("cd")+3:]
-            if path == "..":
-                currentPath = currentPath[currentPath.rfind("/"):]
-            else:
-                if len(currentPath) == 0 or currentPath[-1] == "/" or path == "/":
-                    currentPath += path
-                else:
-                    currentPath += "/" + path
-                if currentPath not in filesystem:
-                    filesystem[currentPath] = {}
+            if path == "..": currentPath = currentPath[:-1]
+            else: 
+                currentPath.append(path)
+                if not currentPath in allPaths:   
+                    allPaths.append(currentPath[:])
     elif line[0].isnumeric():
         size, file = line[:line.index(" ")], line[line.index(" ")+1:]
         allFileSizes += int(size)
-        filesystem[currentPath][file] = int(size)
+        nested_set(filesystem, currentPath + [file], int(size))
 
-print(json.dumps(filesystem,sort_keys=True, indent=4))
+# print(json.dumps(filesystem, sort_keys=True, indent=4))
+
+def recursiveSize(d):
+    size = 0
+    for k in d.keys():
+        if isinstance(d[k], dict):
+            size += recursiveSize(d[k])
+        else:            
+            size += d[k]
+    return size
 
 dirSizes = {}
-for directory in  list(filesystem.keys()):
-    dirSize = 0
-    for file in list(filesystem[directory].keys()):
-        dirSize += filesystem[directory][file]
-    dirSizes[directory] = dirSize
+for path in allPaths:
+    pathString = '/'.join(path)
+    print('/'.join(path) + ": " + str(recursiveSize(getFromDict(filesystem, path))))
+    dirSizes[pathString] = recursiveSize(getFromDict(filesystem, path))
 
-rDirSizes = {}
-for directory in dirSizes.keys():
-    #print(directory + " => " + str(dirSizes[directory]))
-    rDirSizes[directory] = dirSizes[directory]
-    for dir2 in dirSizes.keys():
-        if directory != dir2 and dir2.startswith(directory):
-            #print("+ " + str(dir2) + " - " + str(dirSizes[dir2]))
-            rDirSizes[directory] += dirSizes[dir2]
-    #print("Result: " + directory + " => " + str(rDirSizes[directory]))
+task1Size = 0
+for path in dirSizes.keys():
+    # print(path + ": " + str(dirSizes[path]))
+    if dirSizes[path] <= 100000:
+        task1Size += dirSizes[path]
 
+print(task1Size)
 
-totalSize = 0
-for key in rDirSizes.keys():
-    if rDirSizes[key] <= 100000:
-        totalSize += rDirSizes[key]
+totalFileSize = recursiveSize(filesystem['/'])
+freeSpace = TOTAL_FILESYSTEM - totalFileSize
+neededSpace = UPDATE_SIZE - freeSpace
 
-print(rDirSizes["/"])
-print(allFileSizes)
-print(totalSize)
-
-# 1700687 number is too low
-# 1791835 number is too High
-# 1879303 number is too High
+smallest = TOTAL_FILESYSTEM
+for path in dirSizes.keys():
+    if dirSizes[path] >= neededSpace:
+        if dirSizes[path] <= smallest:
+            smallest = dirSizes[path]
+print('Task 2: ' + str(smallest))
