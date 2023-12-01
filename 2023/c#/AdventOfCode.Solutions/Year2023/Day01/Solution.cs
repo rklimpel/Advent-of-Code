8b655c7f6748130a6326d3e8188e1e79e301266b
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-01 09:22:09 +0100

    add 2023 day 1 solution

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day01/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day01/Solution.cs
@@ -2,15 +2,76 @@ namespace AdventOfCode.Solutions.Year2023.Day01;
 
 class Solution : SolutionBase
 {
-  public Solution() : base(01, 2023, "") { }
+  public Solution() : base(01, 2023, "")
+  {
+  }
 
   protected override string SolvePartOne()
   {
-    return "Test!";
+    string[] lines = Input.Split(
+      new string[] { Environment.NewLine },
+      StringSplitOptions.None
+    );
+
+    int total = 0;
+    
+    foreach (var line in lines)
+    {
+      string numbers = new string(line.Where(char.IsDigit).ToArray());
+      if (numbers.Length >= 1)
+      {
+        int result = Int32.Parse("" + numbers.First() + numbers.Last());
+        total += result;
+      }
+     
+    }
+    return total.ToString();
   }
 
   protected override string SolvePartTwo()
   {
-    return "";
+    string[] numberStrings = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    string[] digitStrings = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    
+    string[] lines = Input.Split(
+      new [] { Environment.NewLine },
+      StringSplitOptions.None
+    );
+
+    int total = 0;
+    
+    foreach (var line in lines)
+    {
+      List<int> numberList = new List<int>();
+
+      for (int i = 0; i < line.Length + 1; i++)
+      {
+        string substring = line.Substring(i, line.Length - i);
+        string matchedNumber = numberStrings.FirstOrDefault(s => substring.StartsWith(s));
+
+        if (matchedNumber != null)
+        {
+          numberList.Add(Array.IndexOf(numberStrings, matchedNumber) + 1);
+        }
+        else
+        {
+          string matchedDigit = digitStrings.FirstOrDefault(s => substring.StartsWith(s));
+
+          if (matchedDigit != null)
+          {
+            numberList.Add(Int32.Parse(matchedDigit));
+          }
+        }
+      }
+
+      if (numberList.Count > 0)
+      {
+        int result = Int32.Parse(numberList.First().ToString() + numberList.Last().ToString());
+        total += result;
+      }
+      
+      
+    }
+    return total.ToString();
   }
 }
