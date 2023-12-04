b641bfa44a34e4148de5e62759672217512af2ca
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-04 08:30:30 +0100

    day 04 1 solved

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day04/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day04/Solution.cs
@@ -1,12 +1,51 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Solutions.Year2023.Day04;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(04, 2023, "") { }
+    private int score = 0;
+    public Solution() : base(04, 2023, "")
+    {
+        string[] lines = Input.Split(
+            new string[] { Environment.NewLine },
+            StringSplitOptions.None
+        );
+        
+
+        foreach (var line in lines)
+        {
+            if (!line.Contains(':')) return;
+            string numberString = line.Split(':')[1];
+            string[] split = numberString.Split('|');
+            
+            var winningNumbers = split[0].Split(" ")
+                .Where(i => i != String.Empty)
+                .Select(i => Int32.Parse(i)).ToList();
+            var myNumberes = split[1].Split(" ")
+                .Where(i => i != String.Empty)
+                .Select(i => Int32.Parse(i)).ToList();
+
+            int matches = 0;
+            foreach (var winningNumber in winningNumbers)
+            {
+                if (myNumberes.Any(x => x == winningNumber))
+                {
+                    matches += 1;
+                }
+            }
+
+            Console.WriteLine("matches: " + matches);
+            Console.WriteLine("add: " + Math.Pow(2, matches - 1));
+            score += (int)Math.Pow(2, matches - 1);
+            
+            Console.WriteLine(score);
+        }
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return score.ToString();
     }
 
     protected override string SolvePartTwo()
