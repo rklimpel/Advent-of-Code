8668f3eb0b7eb5a976e1dd1c84f7579f309f9dcd
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-04 09:35:56 +0100

    prettify day 4 solution

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day04/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day04/Solution.cs
@@ -4,64 +4,52 @@ namespace AdventOfCode.Solutions.Year2023.Day04;
 
 class Solution : SolutionBase
 {
-    private int score = 0;
-    private int[] cardCopies;
-    public Solution() : base(04, 2023, "")
+  private readonly int score = 0;
+  private readonly int[] cardCopies;
+  public Solution() : base(04, 2023, "")
+  {
+    string[] lines = Input.Split(
+        new string[] { Environment.NewLine },
+        StringSplitOptions.None
+    );
+
+    cardCopies = Enumerable.Repeat(1, lines.Length - 1).ToArray();
+
+    for (int i = 0; i < lines.Length; i++)
     {
-        string[] lines = Input.Split(
-            new string[] { Environment.NewLine },
-            StringSplitOptions.None
-        );
-        
-        cardCopies = Enumerable.Repeat(1, lines.Length - 1).ToArray();
+      if (!lines[i].Contains(':')) return;
 
-        for (int i = 0; i < lines.Length; i++)
-        {
-            if (!lines[i].Contains(':')) return;
-            string card = lines[i].Split(':')[0];
-            Console.WriteLine($"process {card}");
-            Console.WriteLine($"Card copies {cardCopies[i]}");
-            string numberString = lines[i].Split(':')[1];
-            string[] split = numberString.Split('|');
-            
-            var winningNumbers = split[0].Split(" ")
-                .Where(i => i != String.Empty)
-                .Select(i => Int32.Parse(i)).ToList();
-            var myNumberes = split[1].Split(" ")
-                .Where(i => i != String.Empty)
-                .Select(i => Int32.Parse(i)).ToList();
+      string[]? split = lines[i]
+          .Split(':')
+          .Skip(1)
+          .Select(s => s.Split('|'))
+          .FirstOrDefault();
 
-            int matches = 0;
-            foreach (var winningNumber in winningNumbers)
-            {
-                if (myNumberes.Any(x => x == winningNumber))
-                {
-                    matches += 1;
-                }
-            }
+      var winningNumbers = split[0].Split(" ")
+          .Where(i => i != string.Empty)
+          .Select(i => int.Parse(i)).ToList();
+      var myNumbers = split[1].Split(" ")
+          .Where(i => i != string.Empty)
+          .Select(i => int.Parse(i)).ToList();
 
-            Console.WriteLine("matches: " + matches);
-            Console.WriteLine("add: " + Math.Pow(2, matches - 1));
-            score += (int)Math.Pow(2, matches - 1);
+      int matches = winningNumbers
+          .Count(winningNumber => myNumbers.Any(x => x == winningNumber));
 
-            int max = i + matches > lines.Length ? lines.Length : i + matches;
-            for (int j = i + 1; j <= max; j++)
-            {
-                cardCopies[j] += cardCopies[i];
-                Console.WriteLine($"Increase CardCopies of Card {j+1} to {cardCopies[j]}");
-            }
-            
-            Console.WriteLine(score);
-        }
+      score += (int)Math.Pow(2, matches - 1);
+
+      int max = i + matches > lines.Length ? lines.Length : i + matches;
+      Enumerable.Range(i + 1, max - i).ToList().ForEach(j => cardCopies[j] += cardCopies[i]);
     }
+  }
 
-    protected override string SolvePartOne()
-    {
-        return score.ToString();
-    }
+  protected override string SolvePartOne()
+  {
+    return score.ToString();
+  }
 
-    protected override string SolvePartTwo()
-    {
-        return cardCopies.Sum().ToString();
-    }
+  protected override string SolvePartTwo()
+  {
+    return cardCopies.Sum().ToString();
+  }
+
 }
