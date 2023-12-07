594799aa257ab99125df0ee39373dc41c56d84aa
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-07 09:56:46 +0100

    solve day 07 part 2

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day07/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day07/Solution.cs
@@ -23,6 +23,23 @@ class Solution : SolutionBase
         {'2', 2}
     };
 
+        Dictionary<char, int> cardStrengthWithJoker = new Dictionary<char, int>
+    {
+        {'A', 14},
+        {'K', 13},
+        {'Q', 12},
+        {'J', 1},
+        {'T', 10},
+        {'9', 9},
+        {'8', 8},
+        {'7', 7},
+        {'6', 6},
+        {'5', 5},
+        {'4', 4},
+        {'3', 3},
+        {'2', 2}
+    };
+
     public Solution() : base(07, 2023, "")
     {
         string[] lines = Input.Split(
@@ -46,9 +63,7 @@ class Solution : SolutionBase
         //Console.WriteLine(string.Join(", ", bidHands.Select(b => b.hand)));
         //Console.WriteLine(string.Join(", ", bidHands.Select(b => b.bid)));
 
-        bidHands.Sort((x, y) => CompareHands(x.hand, y.hand));
-        bidHands.Reverse();
-        Console.WriteLine(string.Join(", ", bidHands.Select(b => b.hand)));
+
 
     }
 
@@ -80,6 +95,35 @@ class Solution : SolutionBase
         return 0;
     }
 
+    private int CompareHandsWithJoker(string handA, string handB)
+    {
+
+        if (GetHandValueWithJoker(handA) > GetHandValueWithJoker(handB))
+        {
+            return -1;
+        }
+        else if (GetHandValueWithJoker(handA) < GetHandValueWithJoker(handB))
+        {
+            return 1;
+        }
+        else
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (cardStrengthWithJoker[handA[i]] > cardStrengthWithJoker[handB[i]])
+                {
+                    return -1;
+                }
+                else if (cardStrengthWithJoker[handA[i]] < cardStrengthWithJoker[handB[i]])
+                {
+                    return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
     private double GetHandValue(string hand)
     {
         var groupCounts = hand
@@ -95,8 +139,29 @@ class Solution : SolutionBase
         return maxCount;
     }
 
+    private double GetHandValueWithJoker(string hand)
+    {
+        int jokerCount = hand.Where(c => c == 'J').Count();
+        var groupCounts = hand
+            .GroupBy(c => c)
+            .Where(c => c.Key != 'J')
+            .Select(g => g.Count())
+            .OrderByDescending(x => x)
+            .ToList();
+        if (groupCounts.Count == 0) return jokerCount;
+        double maxCount = groupCounts[0] + jokerCount;
+        if ((maxCount == 3 || maxCount == 2) && groupCounts[1] == 2)
+        {
+            maxCount += 0.5;
+        }
+        return maxCount;
+    }
+
     protected override string SolvePartOne()
     {
+        bidHands.Sort((x, y) => CompareHands(x.hand, y.hand));
+        bidHands.Reverse();
+
         long result = 0;
         for (int i = 0; i < bidHands.Count; i++)
         {
@@ -107,7 +172,15 @@ class Solution : SolutionBase
 
     protected override string SolvePartTwo()
     {
-        return "";
+        bidHands.Sort((x, y) => CompareHandsWithJoker(x.hand, y.hand));
+        bidHands.Reverse();
+
+        long result = 0;
+        for (int i = 0; i < bidHands.Count; i++)
+        {
+            result += (i+1) * bidHands[i].bid;
+        }
+        return result.ToString();
     }
 }
 
