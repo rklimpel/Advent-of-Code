78bc29fdc3eb5a78d704cc1d19abc8e2e47d076f
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-07 11:52:23 +0100

    play around with c# for day 7

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day07/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day07/Solution.cs
@@ -5,8 +5,7 @@ namespace AdventOfCode.Solutions.Year2023.Day07;
 class Solution : SolutionBase
 {
     List<BidHand> bidHands = [];
-
-    Dictionary<char, int> cardStrength = new Dictionary<char, int>
+    public static Dictionary<char, int> cardStrength = new Dictionary<char, int>
     {
         {'A', 14},
         {'K', 13},
@@ -22,23 +21,8 @@ class Solution : SolutionBase
         {'3', 3},
         {'2', 2}
     };
-
-        Dictionary<char, int> cardStrengthWithJoker = new Dictionary<char, int>
-    {
-        {'A', 14},
-        {'K', 13},
-        {'Q', 12},
-        {'J', 1},
-        {'T', 10},
-        {'9', 9},
-        {'8', 8},
-        {'7', 7},
-        {'6', 6},
-        {'5', 5},
-        {'4', 4},
-        {'3', 3},
-        {'2', 2}
-    };
+    Dictionary<char, int> cardStrengthWithJoker = cardStrength
+        .ToDictionary(pair => pair.Key, pair => pair.Key == 'J' ? 1 : pair.Value);
 
     public Solution() : base(07, 2023, "")
     {
@@ -46,81 +30,37 @@ class Solution : SolutionBase
             new string[] { Environment.NewLine },
             StringSplitOptions.None
         );
-
-        foreach (var line in lines)
-        {
-            if (line != "")
-            {
-                bidHands.Add(
-                    new BidHand(
-                        line.Split(" ")[0],
-                        int.Parse(line.Split(" ")[1])
-                    )
-                );
-            }
-        }
-
-        //Console.WriteLine(string.Join(", ", bidHands.Select(b => b.hand)));
-        //Console.WriteLine(string.Join(", ", bidHands.Select(b => b.bid)));
-
-
-
+        bidHands.AddRange(lines
+            .Where(line => !string.IsNullOrWhiteSpace(line))
+            .Select(line => line.Split(" "))
+            .Select(parts => new BidHand(parts[0], int.Parse(parts[1])))
+        );
     }
 
     private int CompareHands(string handA, string handB)
     {
-        if (GetHandValue(handA) > GetHandValue(handB))
+        int result = GetHandValue(handA).CompareTo(GetHandValue(handB));
+        if (result == 0)
         {
-            return -1;
+            result = handA
+                .Zip(handB, (a, b) => cardStrength[a].CompareTo(cardStrength[b]))
+                .FirstOrDefault(compResult => compResult != 0);
         }
-        else if (GetHandValue(handA) < GetHandValue(handB))
-        {
-            return 1;
-        }
-        else
-        {
-            for (int i = 0; i < handA.Length; i++)
-            {
-                if (cardStrength[handA[i]] > cardStrength[handB[i]])
-                {
-                    return -1;
-                }
-                else if (cardStrength[handA[i]] < cardStrength[handB[i]])
-                {
-                    return 1;
-                }
-            }
-        }
-
-        return 0;
+        return result;
     }
 
     private int CompareHandsWithJoker(string handA, string handB)
     {
-
-        if (GetHandValueWithJoker(handA) > GetHandValueWithJoker(handB))
-        {
-            return -1;
-        }
-        else if (GetHandValueWithJoker(handA) < GetHandValueWithJoker(handB))
-        {
-            return 1;
-        }
+        if (GetHandValueWithJoker(handA) > GetHandValueWithJoker(handB)) return -1;
+        else if (GetHandValueWithJoker(handA) < GetHandValueWithJoker(handB)) return 1;
         else
         {
             for (int i = 0; i < 5; i++)
             {
-                if (cardStrengthWithJoker[handA[i]] > cardStrengthWithJoker[handB[i]])
-                {
-                    return -1;
-                }
-                else if (cardStrengthWithJoker[handA[i]] < cardStrengthWithJoker[handB[i]])
-                {
-                    return 1;
-                }
+                if (cardStrengthWithJoker[handA[i]] > cardStrengthWithJoker[handB[i]]) return -1;
+                else if (cardStrengthWithJoker[handA[i]] < cardStrengthWithJoker[handB[i]]) return 1;
             }
         }
-
         return 0;
     }
 
@@ -161,26 +101,18 @@ class Solution : SolutionBase
     {
         bidHands.Sort((x, y) => CompareHands(x.hand, y.hand));
         bidHands.Reverse();
-
-        long result = 0;
-        for (int i = 0; i < bidHands.Count; i++)
-        {
-            result += (i+1) * bidHands[i].bid;
-        }
-        return result.ToString();
+        return bidHands
+            .Select((bidHand, index) => (index + 1) * bidHand.bid)
+            .Sum().ToString();
     }
 
     protected override string SolvePartTwo()
     {
         bidHands.Sort((x, y) => CompareHandsWithJoker(x.hand, y.hand));
         bidHands.Reverse();
-
-        long result = 0;
-        for (int i = 0; i < bidHands.Count; i++)
-        {
-            result += (i+1) * bidHands[i].bid;
-        }
-        return result.ToString();
+        return bidHands
+            .Select((bidHand, index) => (index + 1) * bidHand.bid)
+            .Sum().ToString();
     }
 }
 
