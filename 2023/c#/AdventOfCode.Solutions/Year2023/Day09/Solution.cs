e6c528e4273ba222fc8c3a6924a4d851b3de563f
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-09 10:08:12 +0100

    solve day 09 part 1

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day09/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day09/Solution.cs
@@ -1,16 +1,76 @@
+using System.Windows.Markup;
+
 namespace AdventOfCode.Solutions.Year2023.Day09;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(09, 2023, "") { }
+    public EnvHistory[] History;
+
+    public Solution() : base(09, 2023, "")
+    {
+        string[] lines = Input.Split(
+            new string[] { Environment.NewLine },
+            StringSplitOptions.None
+        );
+        History = lines
+            .Where(l => l != "")
+            .Select(l => new EnvHistory(l.Split(" ").Select(n => int.Parse(n)).ToArray()))
+            .ToArray();
+
+        foreach (var item in History) item.CalculateDifferences();
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return History.Select(h => h.CalculateFuture()).Sum().ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return History.Select(h => h.CalculatePast()).Sum().ToString();
+    }
+}
+
+class EnvHistory(int[] values)
+{
+    public int[] Values = values;
+    private List<List<int>> differences = [];
+
+    public void CalculateDifferences()
+    {
+        differences.Add(Values.Zip(Values.Skip(1), (a, b) => b - a).ToList());
+        while (!differences.Last().All(v => v == 0))
+        {
+            differences.Add(differences.Last().Zip(differences.Last().Skip(1), (a, b) => b - a).ToList());
+        }
+    }
+
+    public int CalculateFuture()
+    {
+        differences.Last().Add(0);
+        for (int i = differences.Count - 2; i > -1; i--)
+        {
+            Console.WriteLine("Handle differences line " + string.Join(", ", differences[i]));
+            differences[i].Add(differences[i].Last() + differences[i + 1].Last());
+        }
+        PrintDifferences();
+        Console.WriteLine(string.Join(", ", Values));
+        Console.WriteLine("Future: " + (Values.Last() + differences[0].Last()));
+        return Values.Last() + differences[0].Last();
+    }
+
+    public int CalculatePast()
+    {
+        Values.Reverse();   
+        differences.Reverse();
+        return CalculateFuture();
+    }
+
+    private void PrintDifferences()
+    {
+        for (int i = 0; i < differences.Count; i++)
+        {
+            Console.WriteLine(string.Join(" ", differences[i].Select(x => x.ToString().PadLeft(2))));
+        }
     }
 }
