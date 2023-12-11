75c150bbd19fad8daace5e2c8da45b92b27ec30b
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-11 16:22:35 +0100

    sovle day 11 part 2

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day11/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day11/Solution.cs
@@ -5,23 +5,22 @@ class Solution : SolutionBase
     char[][] grid;
     List<Tuple<int, int>> galaxyPositions;
 
+    List<int> emptyRows = new List<int>();
+    List<int> emptyColumns = new List<int>();
+
     public Solution() : base(11, 2023, "")
     {
-        var gridTmp = Input
+        grid = Input
             .Split('\n', StringSplitOptions.RemoveEmptyEntries)
             .Select(line => line.Select(c => c).ToArray())
             .ToArray();
-
-        grid = ExpandGrid(gridTmp);
+        FindExpandRowsAndColumns(grid);
         galaxyPositions = GetGalaxyPositions(grid);
-        PrintGrid(grid);
         Console.WriteLine(string.Join(", ", galaxyPositions));
     }
 
     protected override string SolvePartOne()
     {
-        PrintGrid(grid, addGalaxyNumbers: true);
-
         int[,] distances = new int[galaxyPositions.Count, galaxyPositions.Count];
         for (int i = 0; i < galaxyPositions.Count; i++)
         {
@@ -29,6 +28,24 @@ class Solution : SolutionBase
             {
                 distances[i, j] = Math.Abs(galaxyPositions[i].Item1 - galaxyPositions[j].Item1)
                     + Math.Abs(galaxyPositions[i].Item2 - galaxyPositions[j].Item2);
+
+                foreach (var emptyRow in emptyRows)
+                {
+                    if ((emptyRow > galaxyPositions[i].Item1 && emptyRow < galaxyPositions[j].Item1) ||
+                        (emptyRow > galaxyPositions[j].Item1 && emptyRow < galaxyPositions[i].Item1))
+                    {
+                        distances[i, j] += 1;
+                    }
+                }
+
+                foreach (var emptyColumn in emptyColumns)
+                {
+                    if ((emptyColumn > galaxyPositions[i].Item2 && emptyColumn < galaxyPositions[j].Item2) ||
+                        (emptyColumn > galaxyPositions[j].Item2 && emptyColumn < galaxyPositions[i].Item2))
+                    {
+                        distances[i, j] += 1;
+                    }
+                }
             }
         }
 
@@ -46,14 +63,48 @@ class Solution : SolutionBase
 
     protected override string SolvePartTwo()
     {
-        return "";
+        long[,] distances = new long[galaxyPositions.Count, galaxyPositions.Count];
+        for (int i = 0; i < galaxyPositions.Count; i++)
+        {
+            for (int j = 0; j < galaxyPositions.Count; j++)
+            {
+                distances[i, j] = Math.Abs(galaxyPositions[i].Item1 - galaxyPositions[j].Item1)
+                    + Math.Abs(galaxyPositions[i].Item2 - galaxyPositions[j].Item2);
+
+                foreach (var emptyRow in emptyRows)
+                {
+                    if ((emptyRow > galaxyPositions[i].Item1 && emptyRow < galaxyPositions[j].Item1) ||
+                        (emptyRow > galaxyPositions[j].Item1 && emptyRow < galaxyPositions[i].Item1))
+                    {
+                        distances[i, j] += 1000000 - 1;
+                    }
+                }
+
+                foreach (var emptyColumn in emptyColumns)
+                {
+                    if ((emptyColumn > galaxyPositions[i].Item2 && emptyColumn < galaxyPositions[j].Item2) ||
+                        (emptyColumn > galaxyPositions[j].Item2 && emptyColumn < galaxyPositions[i].Item2))
+                    {
+                        distances[i, j] += 1000000 - 1;
+                    }
+                }
+            }
+        }
+
+        long sum = 0;
+        for (int i = 0; i < galaxyPositions.Count; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                sum += distances[i, j];
+            }
+        }
+
+        return sum.ToString();
     }
 
-    private char[][] ExpandGrid(char[][] originalGrid)
+    private void FindExpandRowsAndColumns(char[][] originalGrid)
     {
-        List<int> emptyRows = new List<int>();
-        List<int> emptyColumns = new List<int>();
-
         for (int i = 0; i < originalGrid.Length; i++)
         {
             if (originalGrid[i].All(c => c != '#'))
@@ -69,79 +120,16 @@ class Solution : SolutionBase
 
         Console.WriteLine("Empty Rows: " + string.Join(", ", emptyRows));
         Console.WriteLine("Empty Columns: " + string.Join(", ", emptyColumns));
-
-        int expandedRows = originalGrid.Length + emptyRows.Count * 2;
-        int expandedColumns = originalGrid[0].Length + emptyColumns.Count * 2;
-
-        List<List<char>> expandedGrid = new();
-
-        for (int i = 0; i < originalGrid.Length; i++)
-        {
-            List<char> row = new();
-
-            for (int j = 0; j < originalGrid[0].Length; j++)
-            {
-                row.Add(originalGrid[i][j]);
-                if (emptyColumns.Contains(j))
-                {
-                    row.Add('.');
-                }
-            }
-
-            expandedGrid.Add(row);
-            if (emptyRows.Contains(i))
-            {
-                expandedGrid.Add(row);
-            }
-        }
-
-        return expandedGrid
-            .Select(row => row.ToArray())
-            .ToArray();
     }
 
     private List<Tuple<int, int>> GetGalaxyPositions(char[][] grid)
     {
-        List<Tuple<int, int>> hashPositions = new List<Tuple<int, int>>();
-        for (int i = 0; i < grid.Length; i++)
-        {
-            for (int j = 0; j < grid[i].Length; j++)
-            {
-                if (grid[i][j] == '#')
-                {
-                    hashPositions.Add(Tuple.Create(i, j));
-                }
-            }
-        }
-        return hashPositions;
+        return grid
+             .SelectMany((row, rowIndex) =>
+                row.Select((cell, colIndex) => new { Cell = cell, Row = rowIndex, Col = colIndex })
+             )
+             .Where(item => item.Cell == '#')
+             .Select(item => Tuple.Create(item.Row, item.Col))
+             .ToList();
     }
-
-    private void PrintGrid(char[][] grid, bool addGalaxyNumbers = false)
-    {
-        for (int i = 0; i < grid.Length; i++)
-        {
-            for (int j = 0; j < grid[i].Length; j++)
-            {
-                if (!addGalaxyNumbers)
-                {
-                    Console.Write(grid[i][j]);
-                }
-                else
-                {
-                    if (grid[i][j] == '#')
-                    {
-                        int galaxyNumber = galaxyPositions.FindIndex(tuple => tuple.Item1 == i && tuple.Item2 == j);
-                        Console.Write(galaxyNumber + 1);
-                    }
-                    else
-                    {
-                        Console.Write(grid[i][j]);
-                    }
-                }
-            }
-            Console.WriteLine();
-        }
-
-    }
-
 }
