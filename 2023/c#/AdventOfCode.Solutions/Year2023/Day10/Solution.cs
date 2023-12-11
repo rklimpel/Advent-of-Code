f0f4538fc653678c50edf3c35c6bdf10757ecbc1
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-11 11:02:29 +0100

    solved day 10git add .! 🔥 finally also part2

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day10/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day10/Solution.cs
@@ -2,142 +2,122 @@ namespace AdventOfCode.Solutions.Year2023.Day10;
 
 class Solution : SolutionBase
 {
-    char[][] graphNodes = [];
-    List<GridPosition>[][] graphVertices = [];
+    static Dictionary<Direction, GridMove> directionMove = new Dictionary<Direction, GridMove>{
+        {Direction.UP, new(-1,0)},
+        {Direction.DOWN, new(1,0)},
+        {Direction.RIGHT, new(0,1)},
+        {Direction.LEFT, new(0,-1)}
+    };
+
+    static Dictionary<Direction, Direction> directionFit = new Dictionary<Direction, Direction>{
+        {Direction.UP, Direction.DOWN},
+        {Direction.DOWN, Direction.UP},
+        {Direction.RIGHT, Direction.LEFT},
+        {Direction.LEFT, Direction.RIGHT}
+    };
+
+    static Dictionary<char, List<Direction>> symbolDirections = new Dictionary<char, List<Direction>>
+    {
+        {'|', new List<Direction> {Direction.UP, Direction.DOWN}},
+        {'-', new List<Direction> {Direction.LEFT, Direction.RIGHT}},
+        {'L', new List<Direction> {Direction.UP, Direction.RIGHT}},
+        {'J', new List<Direction> {Direction.LEFT, Direction.UP}},
+        {'7', new List<Direction> {Direction.LEFT, Direction.DOWN}},
+        {'F', new List<Direction> {Direction.RIGHT, Direction.DOWN}},
+        {'.', new List<Direction>()},
+        {'S', new List<Direction> {Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.UP}},
+    };
+
+    static Direction[] allDirections = (Direction[])Enum.GetValues(typeof(Direction));
+
+    char[][] grid = [];
+    bool[][] finalIsInner = [];
     GridPosition startPosition = new(-1, -1);
-
     List<GridPosition> pipe = new();
-
-    bool?[][] graphNodeIsInner = [];
-
-    Dictionary<char, List<GridMove>> symbolMoves = new Dictionary<char, List<GridMove>>{
-        {'|', new List<GridMove> {new(-1,0), new(1, 0)}},
-        {'-', new List<GridMove> {new(0,-1), new(0, 1)}},
-        {'L', new List<GridMove> {new(-1,0), new(0, 1)}},
-        {'J', new List<GridMove> {new(0,-1), new(-1, 0)}},
-        {'7', new List<GridMove> {new(0,-1), new(1, 0)}},
-        {'F', new List<GridMove> {new(0,1), new(1, 0)}},
-        {'.', new List<GridMove>{}},
-        {'S', new List<GridMove>{new(0,1), new(0,-1), new(1,0), new(-1,0)}},
-    };
-
-    List<GridMove> movesAround = new List<GridMove> {
-        new GridMove(-1,0),
-        new GridMove(1,0),
-        new GridMove(0,-1),
-        new GridMove(0,1)
-    };
 
     public Solution() : base(10, 2023, "")
     {
-        graphNodes = Input
+        grid = Input
             .Split('\n', StringSplitOptions.RemoveEmptyEntries)
             .Select(line => line.Select(c => c).ToArray())
             .ToArray();
-        startPosition = FindStartPosition(graphNodes);
-        graphVertices = FindAllVertices(graphNodes);
+        startPosition = FindStartPosition(grid);
 
-        PrintGraph(graphNodes);
-        PrintGraphVertices(graphVertices);
-        Console.WriteLine(startPosition);
+        PrintGraph(grid);
+        Console.WriteLine("Start Position: " + startPosition);
     }
 
     protected override string SolvePartOne()
     {
-        pipe = FindPipeInGraph(startPosition, graphNodes, graphVertices);
-        PrintPipe(pipe);
+        pipe = FindPipeInGraph(startPosition, grid);
+        PrettyPrintPipe();
         return Math.Ceiling((double)(pipe.Count() - 2) / 2).ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        int innerTileCount = 0;
-        int totalCells = graphNodes.Length * graphNodes[0].Length;
-        int visitedCells = 0;
-
-        graphNodeIsInner = new bool?[graphNodes.Length][];
-
-        for (int i = 0; i < graphNodes.Length; i++)
-        {
-            graphNodeIsInner[i] = new bool?[graphNodes[0].Length];
-        }
-
-        for (int i = 0; i < graphNodes.Length; i++)
-        {
-            for (int j = 0; j < graphNodes[i].Length; j++)
-            {
-                visitedCells++;
-
-                if (!pipe.Contains(new GridPosition(i, j)))
-                {
-                    Console.WriteLine("Test for inner position " + new GridPosition(i, j));
-                    // Possible Inner Tile, becausee it's not part of the loop
-                    // Check every Tile around is part of the Pipe
-                    if (CheckIsInnerPosition(new GridPosition(i, j), new()))
-                    {
-                        graphNodeIsInner[i][j] = true;
-                        innerTileCount += 1;
-                    }
-                    else
-                    {
-                        graphNodeIsInner[i][j] = false;
-                    }
-
-                    double progressPercentage = (double)visitedCells / totalCells * 100;
-                    Console.WriteLine($"Progress: {progressPercentage:F2}%\n");
-                }
-            }
-        }
-
-        PrintIsInnerGrid();
-
-        int trueCount = 0;
-        for (int i = 0; i < graphNodeIsInner.Length; i++)
-        {
-            for (int j = 0; j < graphNodeIsInner[i].Length; j++)
-            {
-                if (graphNodeIsInner[i][j] == true)
-                {
-                    trueCount++;
-                }
-            }
-        }
-
-        Console.WriteLine($"Number of true values: {trueCount}");
-
-        return innerTileCount.ToString();
+        finalIsInner = FindInnerPositions();
+        PrettyPrintPipe();
+        return finalIsInner.Sum(row => row.Count(cell => cell)).ToString();
     }
 
-    private bool CheckIsInnerPosition(GridPosition position, List<GridPosition> alreadyChecked)
+    private bool[][] FindInnerPositions()
     {
-        // Console.WriteLine("Check (recursive) " + position);
-
-        if (position.Row >= graphNodes.Length || position.Row < 0
-            || position.Col >= graphNodes[0].Length || position.Col < 0)
+        bool[][] innerPosition = new bool[grid.Length][];
+        for (int i = 0; i < grid.Length; i++)
         {
-            return false;
+            innerPosition[i] = new bool[grid[i].Length];
         }
 
-        if (graphNodeIsInner[position.Row][position.Col] != null)
+        for (int i = 0; i < pipe.Count - 1; i++)
         {
-            return (bool)graphNodeIsInner[position.Row][position.Col];
-        }
+            GridPosition pos1 = pipe[i];
+            GridPosition pos2 = pipe[i + 1];
+            GridMove move = pos1.CalculateGridMove(pos2);
+            Direction direction = getDirectionByMove(move);
+            Direction whereIsLeft = GetRelativeLeftDirection(direction);
 
-
-        for (int k = 0; k < movesAround.Count; k++)
-        {
-            var testPosition = new GridPosition(position.Row, position.Col).Move(movesAround[k]);
-            if (!alreadyChecked.Contains(testPosition) && !pipe.Contains(testPosition))
+            var possibleInner1 = pos1.Move(directionMove[whereIsLeft]);
+            var possibleInner2 = pos2.Move(directionMove[whereIsLeft]);
+            if (!pipe.Contains(possibleInner1))
             {
-                alreadyChecked.Add(position);
-                if (!CheckIsInnerPosition(testPosition, alreadyChecked))
-                {
-                    return false;
+                innerPosition[possibleInner1.Row][possibleInner1.Col] = true;
+            }
+            if (!pipe.Contains(possibleInner2))
+            {
+                innerPosition[possibleInner2.Row][possibleInner2.Col] = true;
+            }
+
+           
+            innerPosition = FloodFillInner(innerPosition, possibleInner1);
+            innerPosition = FloodFillInner(innerPosition, possibleInner2);
+
+            Console.WriteLine("Moved " + direction);
+        }
+
+        return innerPosition;
+    }
+
+    private bool[][] FloodFillInner(bool[][] innerPositions, GridPosition position)
+    {
+        if (pipe.Contains(position)) return innerPositions;
+        foreach (var direction in allDirections)
+        {
+            GridPosition newPosition = position.Move(directionMove[direction]);
+            if (newPosition.Row < 0 || newPosition.Row >= grid.Length 
+                || newPosition.Col < 0 || newPosition.Col >= grid[0].Length)
+            {
+                continue;
+            }
+            if (innerPositions[newPosition.Row][newPosition.Col] == false)
+            {
+                if (!pipe.Contains(newPosition)){
+                    innerPositions[newPosition.Row][newPosition.Col] = true;
+                    innerPositions = FloodFillInner(innerPositions, newPosition);
                 }
             }
         }
-
-        return true;
+        return innerPositions;
     }
 
     private GridPosition FindStartPosition(char[][] graph)
@@ -152,116 +132,124 @@ class Solution : SolutionBase
         return new(-1, -1);
     }
 
-    private List<GridPosition>[][] FindAllVertices(char[][] graph)
-    {
-        List<GridPosition>[][] graphVertices = new List<GridPosition>[graph.Length][];
-
-        for (int i = 0; i < graph.Length; i++)
-        {
-            graphVertices[i] = new List<GridPosition>[graph[i].Length];
-
-            for (int j = 0; j < graph[i].Length; j++)
-            {
-                graphVertices[i][j] = new List<GridPosition>();
-            }
-        }
-
-        for (int i = 0; i < graph.Length; i++)
-        {
-            for (int j = 0; j < graph[i].Length; j++)
-            {
-                List<GridPosition> vertices = new List<GridPosition>();
-                for (int k = 0; k < symbolMoves[graph[i][j]].Count; k++)
-                {
-                    vertices.Add(new GridPosition(i, j).Move(symbolMoves[graph[i][j]][k]));
-                }
-                graphVertices[i][j] = vertices;
-            }
-        }
-
-        return graphVertices;
-    }
-
     private List<GridPosition> FindPipeInGraph(
         GridPosition startPosition,
-        char[][] graph,
-        List<GridPosition>[][] graphVertices
+        char[][] graph
     )
     {
+        Direction lastMoveDirection = Direction.UP;
         List<GridPosition> pipe = [startPosition];
-        Console.WriteLine("MoveThoughPipe: " + startPosition + " -> " + graphNodes[startPosition.Row][startPosition.Col]);
-        foreach (var item in graphVertices[startPosition.Row][startPosition.Col])
+        foreach (var direction in symbolDirections['S'])
         {
-            Console.WriteLine("Check start in direction " + item);
-            if (graphVertices[item.Row][item.Col].Contains(new GridPosition(startPosition.Row, startPosition.Col)))
+            GridPosition newPosition = startPosition.Move(directionMove[direction]);
+            char gridSymbol = graph[newPosition.Row][newPosition.Col];
+            if (symbolDirections[gridSymbol].Contains(directionFit[direction]))
             {
-                pipe.Add(item);
-                Console.WriteLine("MoveThoughPipe: " + item + " -> " + graphNodes[item.Row][item.Col]);
+                Console.WriteLine("Step " + direction);
+                pipe.Add(newPosition);
+                lastMoveDirection = direction;
                 break;
             }
         }
 
-        return MoveThroughPipe(pipe.Last(), startPosition, 'S', pipe);
+        GridPosition position = pipe.Last();
+        while (grid[pipe.Last().Row][pipe.Last().Col] != 'S')
+        {
+            char gridSymbol = graph[position.Row][position.Col];
+            Console.WriteLine("At " + position + " - " + gridSymbol);
+            Direction direction = symbolDirections[gridSymbol].Where(d => d != directionFit[lastMoveDirection]).First();
+            Console.WriteLine("Step " + direction);
+            position = position.Move(directionMove[direction]);
+            pipe.Add(position);
+            lastMoveDirection = direction;
+
+
+            // PrettyPrintPipeAnimate(pipe, graph);
+        }
+
+        return pipe;
     }
 
-    private List<GridPosition> MoveThroughPipe(
-        GridPosition position,
-        GridPosition lastPosition,
-        char breakChar,
-        List<GridPosition> pipe
-    )
+    private Direction getDirectionByMove(GridMove move)
     {
-        if (graphNodes[position.Row][position.Col] == breakChar) return pipe;
-        GridPosition nextPosition = graphVertices[position.Row][position.Col].Where(x => !x.Equals(lastPosition)).First();
-        pipe.Add(nextPosition);
-        Console.WriteLine("MoveThoughPipe: " + nextPosition + " -> " + graphNodes[nextPosition.Row][nextPosition.Col]);
-        return MoveThroughPipe(nextPosition, position, breakChar, pipe);
+        if (move.Equals(directionMove[Direction.UP])) return Direction.UP;
+        else if (move.Equals(directionMove[Direction.DOWN])) return Direction.DOWN;
+        else if (move.Equals(directionMove[Direction.LEFT])) return Direction.LEFT;
+        else return Direction.RIGHT;
+    }
+
+    public Direction GetRelativeLeftDirection(Direction currentDirection)
+    {
+        switch (currentDirection)
+        {
+            case Direction.UP:
+                return Direction.LEFT;
+            case Direction.DOWN:
+                return Direction.RIGHT;
+            case Direction.RIGHT:
+                return Direction.UP;
+            case Direction.LEFT:
+                return Direction.DOWN;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(currentDirection), currentDirection, "Unknown direction");
+        }
     }
 
     private void PrintGraph(char[][] graph) => graph.ToList().ForEach(line => Console.WriteLine(new string(line)));
     private void PrintPipe(List<GridPosition> pipe) =>
         Console.WriteLine("Pipe:\n" + string.Join("\n", pipe.Select(position => $"({position.Row}, {position.Col})")));
 
-    private void PrintGraphVertices(List<GridPosition>[][] graphVertices)
+    private void PrettyPrintPipe()
     {
-        for (int i = 0; i < graphVertices.Length; i++)
+        for (int i = 0; i < grid.Length; i++)
         {
-            for (int j = 0; j < graphVertices[i].Length; j++)
+            for (int j = 0; j < grid[i].Length; j++)
             {
-                Console.Write($"[{i}][{j}]: ");
-
-                foreach (var vertex in graphVertices[i][j])
-                {
-                    Console.Write($"({vertex.Row}, {vertex.Col}) ");
-                }
-
-                Console.WriteLine();
-            }
-        }
-    }
-
-    private void PrintIsInnerGrid()
-    {
-        for (int i = 0; i < graphNodeIsInner.Length; i++)
-        {
-            for (int j = 0; j < graphNodeIsInner[i].Length; j++)
-            {
-                bool? value = graphNodeIsInner[i][j];
                 string output;
-                if (graphNodeIsInner[i][j] == null) {
-                    if (graphNodes[i][j] == 'L') output = "└";
-                    else if (graphNodes[i][j] == 'F') output = "┌";
-                    else if (graphNodes[i][j] == 'J') output = "┘";
-                    else if (graphNodes[i][j] == '7') output = "┐";
-                    else if (graphNodes[i][j] == '|') output = "|";
-                    else if (graphNodes[i][j] == '-') output = "-";
+                if (pipe.Contains(new GridPosition(i, j)))
+                {
+                    if (grid[i][j] == 'L') output = "└";
+                    else if (grid[i][j] == 'F') output = "┌";
+                    else if (grid[i][j] == 'J') output = "┘";
+                    else if (grid[i][j] == '7') output = "┐";
+                    else if (grid[i][j] == '|') output = "|";
+                    else if (grid[i][j] == '-') output = "-";
+                    else if (grid[i][j] == 'S') output = "S";
                     else output = "?";
                 }
-                else if(graphNodeIsInner[i][j] == true) output = "I";
-                else output = "O";
+                else if (finalIsInner.Length == grid.Length && finalIsInner[i][j]) 
+                {
+                    output = "X";
+                }
+                else {
+                    output = "◙";
+                }
                 Console.Write($"{output,-1}"); // Adjust the width based on your needs
             }
             Console.WriteLine();
+        }
+    }
+
+    private void PrettyPrintPipeAnimate(List<GridPosition> pipe, char[][] grid)
+    {
+        foreach (var position in pipe)
+        {
+            Console.SetCursorPosition(position.Col, position.Row); // Set cursor position to the current pipe position
+
+            string output;
+            if (grid[position.Row][position.Col] == 'L') output = "└";
+            else if (grid[position.Row][position.Col] == 'F') output = "┌";
+            else if (grid[position.Row][position.Col] == 'J') output = "┘";
+            else if (grid[position.Row][position.Col] == '7') output = "┐";
+            else if (grid[position.Row][position.Col] == '|') output = "|";
+            else if (grid[position.Row][position.Col] == '-') output = "-";
+            else if (grid[position.Row][position.Col] == 'S') output = "S";
+            else output = "?";
+
+            Console.Write(output);
+
+            // Introduce a delay between each step (adjust the sleep duration as needed)
+            Thread.Sleep(1); // Sleep for 500 milliseconds (half a second)
         }
     }
 }
@@ -271,6 +259,14 @@ class GridPosition(int row, int col)
     public int Row = row;
     public int Col = col;
     public GridPosition Move(GridMove move) => new GridPosition(Row + move.Row, Col + move.Col);
+
+    public GridMove CalculateGridMove(GridPosition other)
+    {
+        int rowDifference = other.Row - Row;
+        int colDifference = other.Col - Col;
+        return new GridMove(rowDifference, colDifference);
+    }
+
     public override bool Equals(object obj) => obj is GridPosition other && Row == other.Row && Col == other.Col;
     public override string ToString() => $"GraphPosition(Row: {Row}, Col: {Col})";
 }
@@ -279,4 +275,12 @@ class GridMove : GridPosition
 {
     public GridMove(int row, int col) : base(row, col) { }
     public override string ToString() => $"GridMove(Row: {Row}, Col: {Col})";
+}
+
+public enum Direction
+{
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
 }
