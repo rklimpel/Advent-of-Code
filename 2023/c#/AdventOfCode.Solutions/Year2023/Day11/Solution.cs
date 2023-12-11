879b1691d50399a4f349d83d3b0c32116e0c7d0b
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-11 13:14:08 +0100

    day 11 can expand grid

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day11/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day11/Solution.cs
@@ -1,8 +1,23 @@
+using AdventOfCode.Solutions.Year2023.Day10;
+
 namespace AdventOfCode.Solutions.Year2023.Day11;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(11, 2023, "") { }
+    char[][] grid;
+
+    public Solution() : base(11, 2023, "")
+    {
+        var gridTmp = Input
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .Select(line => line.Select(c => c).ToArray())
+            .ToArray();
+
+        grid = ExpandGrid(gridTmp);
+        PrintGrid(grid);
+    }
+
+
 
     protected override string SolvePartOne()
     {
@@ -12,5 +27,68 @@ class Solution : SolutionBase
     protected override string SolvePartTwo()
     {
         return "";
+    }
+
+    private char[][] ExpandGrid(char[][] originalGrid)
+    {
+        List<int> emptyRows = new List<int>();
+        List<int> emptyColumns = new List<int>();
+
+        for (int i = 0; i < originalGrid.Length; i++)
+        {
+            if (originalGrid[i].All(c => c != '#'))
+            {
+                emptyRows.Add(i);
+            }
+
+            if (originalGrid.All(row => row[i] != '#'))
+            {
+                emptyColumns.Add(i);
+            }
+        }
+
+        Console.WriteLine("Empty Rows: " + string.Join(", ", emptyRows));
+        Console.WriteLine("Empty Columns: " + string.Join(", ", emptyColumns));
+
+        int expandedRows = originalGrid.Length + emptyRows.Count * 2;
+        int expandedColumns = originalGrid[0].Length + emptyColumns.Count * 2;
+
+        List<List<char>> expandedGrid = new();
+
+        for (int i = 0; i < originalGrid.Length; i++)
+        {
+            List<char> row = new();
+
+            for (int j = 0; j < originalGrid[0].Length; j++)
+            {
+                row.Add(originalGrid[i][j]);
+                if (emptyColumns.Contains(j)) 
+                {
+                    row.Add('.');
+                }
+            }
+
+            expandedGrid.Add(row);
+            if (emptyRows.Contains(i))
+            {
+                expandedGrid.Add(row);
+            }
+        }
+
+        return expandedGrid
+        .Select(row => row.ToArray())
+        .ToArray();
+    }
+
+    private void PrintGrid(char[][] grid)
+    {
+        foreach (var row in grid)
+        {
+            foreach (var cell in row)
+            {
+                Console.Write(cell);
+            }
+            Console.WriteLine();
+        }
     }
 }
