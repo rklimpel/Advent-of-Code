3b8821ba88f4e9a2d9d0087766baaaea92fe41c2
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-15 13:36:47 +0100

    day 15 part 2 - easy

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day15/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day15/Solution.cs
@@ -1,33 +1,102 @@
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Threading.Channels;
+
 namespace AdventOfCode.Solutions.Year2023.Day15;
 
 class Solution : SolutionBase
 {
-    List<int> hashValues = new();
+    string[] steps;
     public Solution() : base(15, 2023, "")
     {
-        string[] steps = Input
+        steps = Input
             .Split('\n', StringSplitOptions.RemoveEmptyEntries)[0]
             .Split(',').ToArray();
-
-        foreach (var step in steps)
-        {
-            Console.WriteLine(step);
-            var hashResult = Hash(step);
-            hashValues.Add(hashResult);
-            Console.WriteLine(hashResult);
-        }
 
         // Console.WriteLine(Hash("HASH"));
     }
 
     protected override string SolvePartOne()
     {
+        List<int> hashValues = new();
+        foreach (var step in steps)
+        {
+            //Console.WriteLine(step);
+            var hashResult = Hash(step);
+            hashValues.Add(hashResult);
+            //Console.WriteLine(hashResult);
+        }
         return hashValues.Sum().ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        List<string>[] boxes = new List<string>[256];
+        for (int i = 0; i < boxes.Length; i++)
+        {
+            boxes[i] = new List<string>();
+        }
+
+        foreach (var step in steps)
+        {
+            if (step.Contains('='))
+            {
+                string label = step.Split('=')[0];
+                int boxId = Hash(label);
+                string focalLength = step.Split('=')[1];
+
+
+                if (boxes[boxId].Where(x => x.Contains(label)).ToList().Count() == 0)
+                {
+                    boxes[boxId].Add($"{label} {focalLength}");
+                }
+                else
+                {
+                    for (int i = 0; i < boxes[boxId].Count(); i++)
+                    {
+                        if (boxes[boxId][i].Contains(label))
+                        {
+                            boxes[boxId][i] = $"{label} {focalLength}";
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                var label = step.Split('-')[0];
+                int boxId = Hash(label);
+                if (boxes[boxId].Where(x => x.Contains(label)).ToList().Count() != 0)
+                {
+                    for (int i = 0; i < boxes[boxId].Count(); i++)
+                    {
+                        if (boxes[boxId][i].Contains(label))
+                        {
+                            boxes[boxId].Remove(boxes[boxId][i]);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < boxes.Length; i++)
+        {
+            Console.WriteLine($"Box {i}: {string.Join(", ", boxes[i].Select(x => $"[{x}] "))}");
+        }
+
+        long focusPower = 0;
+        for (int boxId = 0; boxId < boxes.Length; boxId++)
+        {
+            for (int slotId = 0; slotId < boxes[boxId].Count; slotId++)
+            {
+                int focalLength = int.Parse(boxes[boxId][slotId].Split(" ")[1]);
+                int lensFocusPower = (boxId + 1) * (slotId + 1) * focalLength;
+                focusPower += lensFocusPower;
+            }
+        }
+
+
+        return focusPower.ToString();
     }
 
     private int Hash(string step)
