85134a1ce8b8420d7012cddbda58499bd408f3d2
Rico Klimpel <rklimpel@slashwhy.de>
2023-12-15 12:46:58 +0100

    day 15 part 1 - super easy

--- 2023/c#/AdventOfCode.Solutions/Year2023/Day15/Solution.cs
+++ 2023/c#/AdventOfCode.Solutions/Year2023/Day15/Solution.cs
@@ -2,15 +2,43 @@ namespace AdventOfCode.Solutions.Year2023.Day15;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(15, 2023, "") { }
+    List<int> hashValues = new();
+    public Solution() : base(15, 2023, "")
+    {
+        string[] steps = Input
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)[0]
+            .Split(',').ToArray();
+
+        foreach (var step in steps)
+        {
+            Console.WriteLine(step);
+            var hashResult = Hash(step);
+            hashValues.Add(hashResult);
+            Console.WriteLine(hashResult);
+        }
+
+        // Console.WriteLine(Hash("HASH"));
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return hashValues.Sum().ToString();
     }
 
     protected override string SolvePartTwo()
     {
         return "";
+    }
+
+    private int Hash(string step)
+    {
+        int currentValue = 0;
+        for (int i = 0; i < step.Length; i++)
+        {
+            currentValue += (int)step[i];
+            currentValue *= 17;
+            currentValue = currentValue % 256;
+        }
+        return currentValue;
     }
 }
